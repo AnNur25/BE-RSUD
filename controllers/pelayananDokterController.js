abf3a956e914c0cefb85a213404f4666edb8c93a
ahazain <ahmadfariddd5@gmail.com>
2025-03-17 20:35:34 +0700

    CRUD pelayanan-dokter

--- controllers/pelayananDokterController.js
+++ controllers/pelayananDokterController.js
@@ -1,20 +1,64 @@
 const prisma = require("../prisma/prismaClient");
 
 class PelayananDokterController {
-    static async addPelayananDokter(req, res) {
-        const { nama_pelayanan, deskripsi } = req.body;
-        try {
-            const pelayananDokter = await prisma.pelayananDokter.create({
-                data: {
-                    nama_pelayanan,
-                    deskripsi
-                }
-            });
-            res.json(pelayananDokter);
-        } catch (error) {
-            res.json({ error: error.message });
-        }
+  static async createPelayananDokter(req, res) {
+    const { nama_pelayanan, deskripsi } = req.body;
+    const userId = req.user.id_user;
+    try {
+      const pelayananDokter = await prisma.pelayananDokter.create({
+        data: {
+          nama_pelayanan,
+          deskripsi,
+          user: {
+            connect: { id_user: userId },
+          },
+        },
+      });
+      res.json(pelayananDokter);
+    } catch (error) {
+      res.json({ error: error.message });
     }
+  }
 
+  static async getPelayananDokter(req, res) {
+    try {
+      const pelayananDokter = await prisma.pelayananDokter.findMany({
+        select: {
+          id_pelayanan_dokter: true,
+          nama_pelayanan: true,
+          deskripsi: true,
+        },
+      });
+      res.json(pelayananDokter);
+    } catch (error) {
+      res.json({ error: error.message });
+    }
+  }
+
+  static async updatePelayananDokter(req, res) {
+    const { id } = req.params;
+    const { nama_pelayanan, deskripsi } = req.body;
+    try {
+      const pelayananDokter = await prisma.pelayananDokter.update({
+        where: { id_pelayanan_dokter: parseInt(id) },
+        data: { nama_pelayanan, deskripsi },
+      });
+      res.json(pelayananDokter);
+    } catch (error) {
+      res.json({ error: error.message });
+    }
+  }
+
+  static async deletePelayananDokter(req, res) {
+    const { id } = req.params;
+    try {
+      const pelayananDokter = await prisma.pelayananDokter.delete({
+        where: { id_pelayanan_dokter: parseInt(id) },
+      });
+      res.json(pelayananDokter);
+    } catch (error) {
+      res.json({ error: error.message });
+    }
+  }
 }
 module.exports = PelayananDokterController;
--- routes/pelayananDokterRoute.js
+++ routes/pelayananDokterRoute.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const Route = express.Router();
 const pelayananDokterController = require("../controllers/pelayananDokterController");
+const { auth } = require("../middleware/authMiddleware");
+
+Route.post("/", auth, pelayananDokterController.createPelayananDokter);
+Route.get("/", auth, pelayananDokterController.getPelayananDokter);
+Route.put("/:id", pelayananDokterController.updatePelayananDokter);
+Route.delete("/:id", pelayananDokterController.deletePelayananDokter);
 
 
 module.exports = Route;
