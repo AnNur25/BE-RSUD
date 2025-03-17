ddc71e38ab3e1a8f48ec6e34eb96209e8d5651fc
ahazain <ahmadfariddd5@gmail.com>
2025-03-17 20:20:32 +0700

    crud spesialis

--- routes/spesialisRoute.js
+++ routes/spesialisRoute.js
@@ -4,5 +4,8 @@ const spesialisController = require("../controllers/spesialisCoontroller");
 const { auth } = require("../middleware/authMiddleware");
 
 Route.post("/", auth, spesialisController.createSpesialis);
+Route.get("/", spesialisController.getSpesialis);
+Route.put("/:id", spesialisController.updateSpesialis);
+Route.delete("/:id", spesialisController.deleteSpesialis);
 
 module.exports = Route;
